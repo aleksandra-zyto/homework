a98230e89c5c2002e6a7900d45dd9921c109bb87
ola-zyto <ola.zyto606@gmail.com>
2025-06-22 21:52:55 +0100

    remove icon

--- fe/src/components/Header/DashboardHeader.tsx
+++ fe/src/components/Header/DashboardHeader.tsx
@@ -3,23 +3,6 @@ import { useAuth } from "../../contexts/AuthContext";
 import styles from "./DashboardHeader.module.scss";
 import { Button } from "../Button/Button";
 
-const RefreshIcon = () => (
-  <svg
-    fill="none"
-    stroke="currentColor"
-    viewBox="0 0 24 24"
-    width="20"
-    height="20"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-    />
-  </svg>
-);
-
 interface DashboardHeaderProps {
   onRefresh: () => void;
 }
